5b9b8bb71b66e84f8ed92555e5cee9d2270ff33f
Johan Geluk <johan@geluk.io>
2023-12-03 22:59:09 +0100

    Add timing information

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2023
 
 My solutions for Advent of Code 2023 (https://adventofcode.com/).
+
+Guaranteed 100% 🔥blazing fast🔥 or your money back.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use input::DayInput;
 use solutions::*;
 
@@ -13,18 +15,33 @@ trait Day {
 }
 
 fn main() {
+    let now = Instant::now();
+
     solve_day::<Day1>();
     solve_day::<Day2>();
     solve_day::<Day3>();
+
+    let elapsed = now.elapsed().as_micros() as f32 / 1000.0;
+
+    println!("🔥blazing fast🔥: all solutions calculated in {elapsed} ms",)
 }
 
 fn solve_day<D: Day>() {
-    let input = input::load_day(D::DAY_NO);
+    let (input, _) = measure(|| input::load_day(D::DAY_NO));
 
-    println!("Day {}: ", D::DAY_NO);
-    let answer_1 = D::solve_challenge_1(&input);
-    println!(" - first answer: {answer_1}");
+    let (answer_1, t_1) = measure(|| D::solve_challenge_1(&input));
+    let (answer_2, t_2) = measure(|| D::solve_challenge_2(&input));
 
-    let answer_2 = D::solve_challenge_2(&input);
-    println!(" - second answer: {answer_2}");
+    println!("Day {}:", D::DAY_NO);
+    println!(" - first answer: {answer_1} (in {}µs)", t_1.as_micros());
+    println!(" - second answer: {answer_2} (in {}µs)", t_2.as_micros());
+}
+
+fn measure<F, R>(action: F) -> (R, Duration)
+where
+    F: FnOnce() -> R,
+{
+    let now = Instant::now();
+    let res = action();
+    (res, now.elapsed())
 }
