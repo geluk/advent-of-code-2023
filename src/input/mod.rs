01acaebb5c935872d8cc93fc54a209607efca8f7
Johan Geluk <johan@geluk.io>
2023-12-08 20:22:32 +0100

    Add day 7

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,10 +1,11 @@
-const DAYS: [&str; 6] = [
+const DAYS: [&str; 7] = [
     include_str!("day01.txt"),
     include_str!("day02.txt"),
     include_str!("day03.txt"),
     include_str!("day04.txt"),
     include_str!("day05.txt"),
     include_str!("day06.txt"),
+    include_str!("day07.txt"),
 ];
 
 pub fn load_day<I: DayInput>(day_no: usize) -> I {
